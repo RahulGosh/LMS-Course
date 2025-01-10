5a106d5b604e7830bb7d1aee33c93fa2f5a17701
Yash Puniwala <yashpuniwala@gmail.com>
2025-01-10 23:46:47 +0530

    new

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: "https://lms-course-iota.vercel.app",
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }
 app.use(cors(corsOptions));
